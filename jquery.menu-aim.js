1fd0fbc3c1d6cf2f110341711eb91de9a01275d8
Dan Abrey <danabrey@gmail.com>
2017-02-14 11:42:40 +0000

    Update jquery.menu-aim.js

--- jquery.menu-aim.js
+++ jquery.menu-aim.js
@@ -95,18 +95,14 @@
                 exit: $.noop,
                 activate: $.noop,
                 deactivate: $.noop,
-                exitMenu: $.noop
+                exitMenu: $.noop,
+                clearActiveRow: function() {
+                    activeRow = null;
+                }
             }, opts);
 
         var MOUSE_LOCS_TRACKED = 3,  // number of past mouse locations to track
             DELAY = 300;  // ms delay when user appears to be entering submenu
-        
-        /**
-         * Clears the active row, for use with asynchronous callbacks, e.g. setTimeout
-         */
-        this.clearActiveRow = function() {
-            activeRow = null;
-        }
 
         /**
          * Keep track of the last few locations of the mouse.
